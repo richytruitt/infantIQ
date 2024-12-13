87c8ea1e9b8086afe496d48fe8abd63635a895b9
richytruitt <32624695+richytruitt@users.noreply.github.com>
2024-12-13 09:18:11 -0600

    saving values to dynamodb

--- features/iot_core_to_dynamodb/main.py
+++ features/iot_core_to_dynamodb/main.py
@@ -1,5 +1,17 @@
+import boto3
+
 
 def lambda_handler(event, context):
-    print("hello world")
-    print(f"EVENT: {event}")
-    print(f"CONTEXT: {context}")
+    dynamodb = boto3.resource('dynamodb')
+
+    table = dynamodb.Table('actions')
+    response = table.put_item(
+        Item={
+            'timestamp': str(event["timestamp"]),
+            'deviceID': str(event["deviceID"]),
+            'action': str(event["action"])
+        }
+    )
+    status_code = response['ResponseMetadata']['HTTPStatusCode']
+    print(status_code)
+    print("Execution FInished")
